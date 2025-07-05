6332a0426309f7f8f9cf18a21a753452d8af59bd
Jonas <jhoucdn@gmail.com>
2025-07-05 07:52:00 -0400

    Implementacao do post date nas pages

--- page/models.py
+++ page/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import CustomUser
+from datetime import date
 
 class Page(models.Model):
     author = models.ForeignKey(CustomUser, null=True, on_delete=models.CASCADE)
@@ -7,3 +8,11 @@ class Page(models.Model):
     content = models.TextField(max_length=900)
     publication_date = models.DateTimeField(auto_now_add=True)
     public_access = models.BooleanField(default=False)
+    public_access_date = models.DateField()
+    
+    def save(self, *args, **kwargs):
+        if self.public_access_date < date.today():
+            self.public_access = True
+        
+        super().save(*args, **kwargs)
+        
