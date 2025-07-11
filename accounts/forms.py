e0cc0b94b35fc234df94c221a4d4444a554fd565
Jonas <jhoucdn@gmail.com>
2025-07-11 10:33:44 -0400

    Ajustes no template

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from accounts.models import CustomUser
+from diary import settings
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class CustomUserForm(UserCreationForm):
@@ -21,7 +22,7 @@ class CustomUserForm(UserCreationForm):
     
     class Meta:
         model = CustomUser
-        fields = ['email', 'myname', 'birthday', 'password1']
+        fields = ['email', 'myname', 'birthday']
         widgets = {
             'myname': forms.TextInput(attrs={"class": "bg-stone-100 px-4 py-2 outline-none rounded-md w-full", "placeholder": "Type your name"}),
             'birthday': forms.DateInput(attrs={"class": "bg-stone-100 px-4 py-2 outline-none rounded-md w-full", "placeholder": "Ex: 19/11/2005"}),
